b99c116baf0f880a00a7e73ca3f3efbb34bdd6ba
Hyacent Muripaga <muripaga.321477@baguio.sti.edu.ph>
2025-03-06 08:19:27 +0800

    Add files via upload

---
+++ New Lesson Midterm 02/script.js
@@ -0,0 +1,8 @@
+console.log("BAsic");
+document.write("WALA TO") // ni lalagay ito sa body -- isang html na file siya na ininsert sa html 
+
+class bankLoan {
+    static calcPayemtn(x){
+        return x
+    }
+}
